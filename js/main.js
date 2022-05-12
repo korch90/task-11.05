4dc386e7cdc9d557a80cbf188a2e118e26cfbc41
Andrian Korchinsky <andriankorchinsky@MacBook-Pro-Andrian.local>
2022-05-12 18:07:51 +0300

    f

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
-let fib=(n)=>{
-    
-}
+// Task1
+//     let fab=(n)=>{
+// let a=1
+// let b=1
+// let c
+// let arr=[1,1]
+// for(let i=1;i<=n;i++){
+//     c=a+b
+//     a=b
+//     b=c
+//     arr.push(c)
+// } 
+// console.log(arr)
+// }
+// fab(7)
+
+
+
+
+// task2
+// let showSum=function(q,n){
+// let b1=1
+// let sum=b1*(n**q - 1)/(n-1)
+// return sum
+// }
+// console.log(showSum(5,2))
+
+// let showSum=function(q,n){
+// return (n==1)? 1: n*showSum(q-1,n)
+// }
+// console.log(showSum(5,2))
+
+// Task3
+//  let a=prompt("first number")
+//  let b=prompt("second number")
+// let arr=[]
+
+// let func=function(a,b){
+// for(;a<b;a++){
+// for(i=2;i<a;i++){
+//     if(a%i==0){
+//      break
+//     }
+//     else if(i==(a-1)) {
+//         arr.push(a) 
+//     }
+//   }
+// }
+// }
+// func(a,b)
+// console.log(arr)  //////перше число стрінга????
